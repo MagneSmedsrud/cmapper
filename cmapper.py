f61a5e9ada06a46a27772ca1eaf980af4d0d6639
Magne Thorsell Smedsrud <msmedsrud@outlook.com>
2020-12-01 17:25:16 +0100

    Added function to convert single hex code to decimals

--- cmapper.py
+++ cmapper.py
@@ -5,7 +5,26 @@ import argparse
 
 
 #convert hashes to 0-1s
+def convert_hex_dec(color):
+    """
+    converts color hex code in format #RRGGBB into three decimals between 0 and 1
 
+    PARAMETERS
+    color: string of hex color code in format #RRGGBB 
+
+    RETURNS
+    red, green, blue: Each component with values between 0 and 1
+    """
+
+    #Strip # from color hex
+    color = color.strip("#")
+
+    #Turn each hex calue RRGGBB into integer and divide by 256 to decimals
+    red = int(color[:2], 16) /256
+    green = int(color[2:4], 16) /256
+    blue = int(color[4:], 16) /256
+
+    return red, green, blue
 
 #def create cmap
 #list of hashes
@@ -31,9 +50,9 @@ def arg_parser():
 #main function
 def main():
     args = arg_parser()
-    for a in args.color_list:
-        print(type(int(a)))
-
+    
+    #running tests
+    convert_hex_dec("#a5a8b6")
 
 if __name__ == "__main__":
     main()
