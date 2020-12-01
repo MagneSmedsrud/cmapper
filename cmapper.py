06dc1431cc7f900c703fcf05ee12ea2fd64cc95f
Magne Thorsell Smedsrud <msmedsrud@outlook.com>
2020-12-01 10:27:43 +0100

    arg parser

--- cmapper.py
+++ cmapper.py
@@ -1,1 +1,39 @@
-print("hello world!")
+#Imports
+import argparse
+
+# import matplot lib + colors
+
+
+#convert hashes to 0-1s
+
+
+#def create cmap
+#list of hashes
+# len list = something about number of entries
+
+#later: discrete list (not continuous) - input from 
+
+
+#Parse Arguments
+def arg_parser():
+    #setting up argument parser
+    parser = argparse.ArgumentParser(description='Create custom cmap from list of color hexes')
+    
+    #create arguments
+    parser.add_argument("color_list", nargs="+", help="list of color hex values")
+
+    #parse arguments to be returned
+    args = parser.parse_args()
+    
+    return args
+
+
+#main function
+def main():
+    args = arg_parser()
+    for a in args.color_list[0]:
+        print(type(a))
+
+
+if __name__ == "__main__":
+    main()
